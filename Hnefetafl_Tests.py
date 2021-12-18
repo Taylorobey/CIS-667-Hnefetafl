708b5f9dd57ed96cebd1a912f96edc7f1872edbe
Taylor Douglas Robey <taylor.d.robey@gmail.com>
2021-12-18 11:09:51 -0500

    Minor formatting change

--- Hnefetafl_Tests.py
+++ Hnefetafl_Tests.py
@@ -11,13 +11,6 @@ CHILDREN_PER_NODE = 5
 WIN_WEIGHT = 3
 LEARN_RATE = 2
 
-#Conduct 5 experiments, one for each problem size.
-#For each problem size, simulate 100 games, in which the baseline AI plays against the tree-based AI.  If the domain is single-player, run 100 games with the baseline and another 100 with the tree-based AI.
-#Make sure the 100 games are not all identical (for example, using different initial states, or playing against a non-deterministic baseline).
-#Record the number of nodes processed by the tree-based AI over the course of each game.
-#Record the final score for the tree-based and baseline AIs at the end of each game.
-#Create two histograms for each problem size: one showing the distribution of node counts, and one showing the distribution of final scores for each AI, across the 100 games.
-
 def test_play(board_size, max_depth, children_per_node, win_weight, learn_rate, main_player, net=None):
     # The attackers' side moves first; the players then take turns.
     players = [main_player,1]
@@ -98,9 +91,7 @@ def tree_test():
     return 0
 
 def NN_test():
-    #Generating a training and testing dataset, each with at least 500 examples. Each example should be a random game state, encoded as an array of numbers (used as input for the NN), paired with some target output for the NN, such as the utility or heuristic value of the game state.
-
-    #Configure and train the neural network on the training data, and evaluate on the test data, for at least 500 gradient descent weight updates. You should evaluate on the test data every update, but only the training data should be used to calculate the error gradient with respect to the weights. Plot the learning curve showing update iteration on the x-axis and error on the y-axis (one curve for training error, one for test error).
+    #train neural network
     net = cnn.CNN(11, 4)
 
     train_curve, test_curve = cnn.train_network(net, 100, 11, 1)
@@ -113,7 +104,7 @@ def NN_test():
     plt.ylabel('Error')
     plt.show()
 
-    #After training, simulate 100 games using the trained NN incorporated into the tree-based AI. Play it against the baseline AI if your game is two-player. Record and plot the same data as before: number of nodes processed and final scores for each AI. Again, make sure the 100 games are not all identical.
+    #simulate 100 games
     net_nodes = []
     simple_nodes = []
     net_scores = []
@@ -142,4 +133,4 @@ def NN_test():
 
 #uncomment to run tests
 tree_test()
-#NN_test()
+NN_test()
